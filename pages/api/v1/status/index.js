1d4ae4e5de26ab7d0f498762ba014db0f5fd4c15
Willian <willianrochati@gmail.com>
2025-10-28 21:36:59 -0300

    add `database.js` and `.env`files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,9 @@
-function status(request, response) {
-  response
-    .status(200)
-    .json({ chave: "viva cada dia como se fosse o ultimo dia" });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1;");
+  console.log(result);
+  response.status(200).json({ chave: "são acima da média as sum: 2" });
 }
+
 export default status;
