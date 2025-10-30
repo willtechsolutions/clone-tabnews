a6f334f117f018e861bd3351d75b0f775f5b0257
Willian <willianrochati@gmail.com>
2025-10-30 20:40:36 -0300

    add `jsconfig.json file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,9 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
+  const result = await database.query("SELECT 1 + 1 as sum");
   console.log(result);
-  response.status(200).json({ chave: "são acima da média as sum: 2" });
+  response.status(200).json({ chave: "são acima da média" });
 }
 
 export default status;
